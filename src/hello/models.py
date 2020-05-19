aaeb8adad605844ecbeaafea143b842c5db7aa50
yasinkokturk <myasinkokturk@gmail.com>
2020-05-20 00:25:47 +0300

    Missing colons added

--- src/hello/models.py
+++ src/hello/models.py
@@ -8,28 +8,28 @@ class User(models.Model):
     password = models.TextField(max_length=64, null=False)
     email = models.TextField(max_length=64, unique=True, null=False)
 
-class User_phone_number(models.Model)
+class User_Phone_Number(models.Model):
     id = models.ForeignKey('User', on_delete=models.CASCADE)
     phone_number = models.TextField(max_length=64,  null=False)
     class Meta:
         unique_together = [['id', 'phone_number']]
 
-class Customer(User)
+class Customer(User):
     
 
-class Seller(User)
+class Seller(User):
     shop_name = models.TextField(max_length=64, unique=True, null=False)
     rating = models.DecimalField(max_digits=3, decimal_places=1)
     address = models.TextField(max_length=64, unique=True, null=False)
 
 
-class Courier(User)
+class Courier(User):
     city = models.TextField(max_length=64, null = False)
     vehicle = models.TextField(max_length=64)
 
-class Flower(models.Model)
+class Flower(models.Model):
     id = models.IntegerField(primary_key=True)
-    type = models.TextField(max_length=64, null=False)
+    flower_type = models.TextField(max_length=64, null=False)
     color = models.TextField(max_length=64, null=False)
     occasion = models.TextField(max_length=64, unique=True, null=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
